84e6574a2cad58f4bf89872a555f7521f36367cb
Arturo GUerrero <arturo.guerrero@koibanx.com>
2023-05-25 14:43:11 -0600

    delete docusaurios

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require('/home/arturo/git/documentacion/crypto-checkout-docs/node_modules/infima/dist/css/default/default.css'),
-  require('/home/arturo/git/documentacion/crypto-checkout-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/arturo/git/documentacion/crypto-checkout-docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/arturo/git/documentacion/crypto-checkout-docs/src/css/custom.css'),
-  require('/home/arturo/git/documentacion/crypto-checkout-docs/src/css/api-reference.css'),
-  require('/home/arturo/git/documentacion/crypto-checkout-docs/src/client/define-ui-kit.js'),
-];
